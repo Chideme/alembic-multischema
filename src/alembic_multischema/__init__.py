d23bef964cafa1bba6c9d754a315d3562cbe9468
Chideme <kudakwashechideme@gmail.com>
2020-08-23 16:24:08 +0200

    include all system schemas

--- src/alembic_multischema/__init__.py
+++ src/alembic_multischema/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import text
 def getNonSystemSchemas(exclude=[]):
     try:
       conn = op.get_bind()
-      schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'pg_catalog')")).fetchall()
+      schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'pg_catalog','pg_toast', 'pg_temp_1', 'pg_toast_temp_1')")).fetchall()
       return [ str(x[0]) for x in schemas if x not in exclude]
     except:
       return []
@@ -20,14 +20,15 @@ def getSchemas(exclude=[]):
 def perSchema(schemas=getNonSystemSchemas(), exclude=[]):
   def externalWrapper(func):
     def wrapper(*args, **kwargs):
-      for s in schemas if s not in exclude:
-        print("Executing on schema {}".format(s))
-        try:
-          op.execute(text("SET search_path TO {}".format(s)))
-          func(*args, **kwargs)
-          op.execute(text("SET search_path TO default"))
-        except Exception as e:
-          op.execute(text("SET search_path TO default"))
-          raise e
+      for s in schemas:
+        if s not in exclude:
+          print("Executing on schema {}".format(s))
+          try:
+            op.execute(text("SET search_path TO {}".format(s)))
+            func(*args, **kwargs)
+            op.execute(text("SET search_path TO default"))
+          except Exception as e:
+            op.execute(text("SET search_path TO default"))
+            raise e
     return wrapper
   return externalWrapper
